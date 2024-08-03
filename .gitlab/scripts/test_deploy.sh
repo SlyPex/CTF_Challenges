bb1b585382c55b4792b9c8104d555b3d0e9c63a9
Slimane MEHARZI <59923212+SlyPex@users.noreply.github.com>
2024-08-03 10:59:25 +0100

    Restructured Whole Project

--- .gitlab/scripts/test_deploy.sh
+++ .gitlab/scripts/test_deploy.sh
@@ -6,14 +6,14 @@ CHALL_NAME=$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
 
 if [[ $DEPLOY_ENV = "Dockerfile" ]]
 then
-	docker buildx build ./$CHALL_NAME/ --load -t "$(echo $CHALL_NAME | tr '[A-Z]' '[a-z]')":latest
+	docker buildx build $DIRECTORY/$CHALL_NAME/ --load -t "$(echo $CHALL_NAME | tr '[A-Z]' '[a-z]')":latest
 	docker run -d "$(echo $CHALL_NAME | tr '[A-Z]' '[a-z]')":latest
 	docker ps -a
 	docker stop $(docker ps -q)
 elif [[ $DEPLOY_ENV = "docker-compose" ]]
 then
-	docker compose -f $CHALL_NAME/docker-compose.yml up -d --build
+	docker compose -f $DIRECTORY/$CHALL_NAME/docker-compose.yml up -d --build
 	docker ps -a
 	sleep 5
-	docker compose -f $CHALL_NAME/docker-compose.yml down
+	docker compose -f $DIRECTORY/$CHALL_NAME/docker-compose.yml down
 fi
