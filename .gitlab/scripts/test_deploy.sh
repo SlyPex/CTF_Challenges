74a658cf95749d443fe362efac04b32ad05e3454
Slimane MEHARZI <59923212+SlyPex@users.noreply.github.com>
2024-08-03 19:15:55 +0100

    Updated Gitlab Pipeline configuration file (.gilab-ci.yml)

--- .gitlab/scripts/test_deploy.sh
+++ .gitlab/scripts/test_deploy.sh
@@ -2,18 +2,19 @@
 
 set -euo pipefail
 
-CHALL_NAME=$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
+CHALL_PATH=$1
+CHALL_NAME="$(echo $1 | cut -d '/' -f 2)"
 
 if [[ $DEPLOY_ENV = "Dockerfile" ]]
 then
-	docker buildx build $DIRECTORY/$CHALL_NAME/ --load -t "$(echo $CHALL_NAME | tr '[A-Z]' '[a-z]')":latest
+	docker buildx build $CHALL_PATH/ --load -t "$(echo $CHALL_NAME | tr '[A-Z]' '[a-z]')":latest
 	docker run -d "$(echo $CHALL_NAME | tr '[A-Z]' '[a-z]')":latest
 	docker ps -a
 	docker stop $(docker ps -q)
 elif [[ $DEPLOY_ENV = "docker-compose" ]]
 then
-	docker compose -f $DIRECTORY/$CHALL_NAME/docker-compose.yml up -d --build
+	docker compose -f $CHALL_PATH/docker-compose.yml up -d --build
 	docker ps -a
 	sleep 5
-	docker compose -f $DIRECTORY/$CHALL_NAME/docker-compose.yml down
+	docker compose -f $CHALL_PATH/docker-compose.yml down
 fi
