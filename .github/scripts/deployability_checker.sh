106045608ada217e675a7045ffcc89a1cb5ff68b
Slimane MEHARZI <59923212+SlyPex@users.noreply.github.com>
2024-08-04 15:18:13 +0100

    Updated Bash scripts, redirect errors to stderr

--- .github/scripts/deployability_checker.sh
+++ .github/scripts/deployability_checker.sh
@@ -4,16 +4,16 @@ set -euo pipefail
 
 if  [[ "$#" -gt 1 ]]
 then
-	echo -e "[!] - ERROR : Extra Argument Specified\nUsage : $0 [path_to_directory]" >2
+	echo -e "[!] - ERROR : Extra Argument Specified\nUsage : $0 [path_to_directory]" >&2
 	exit 1
 elif [[ "$#" -ne 1 ]]
 then
-	echo -e "[!] - ERROR : Missing Argument\nUsage : $0 [path_to_directory]" >2
+	echo -e "[!] - ERROR : Missing Argument\nUsage : $0 [path_to_directory]" >&2
 	exit 1
 else
 	if [[ ! -d "$1" ]] 
 	then
-		echo "[!] - ERROR : The specified Argument $1 is not a directory" >2
+		echo "[!] - ERROR : The specified Argument $1 is not a directory" >&2
 		exit 1
 	elif (ls -lA $1 | grep -qo docker-compose.yml)
 	then
--- .gitlab/scripts/deployability_checker.sh
+++ .gitlab/scripts/deployability_checker.sh
@@ -6,16 +6,16 @@ OUTPUT_FILE="deploy.env"
 
 if  [[ "$#" -gt 1 ]]
 then
-	echo -e "[!] - ERROR : Extra Argument Specified\nUsage : $0 [path_to_directory]" >2
+	echo -e "[!] - ERROR : Extra Argument Specified\nUsage : $0 [path_to_directory]" >&2
 	exit 1
 elif [[ "$#" -ne 1 ]]
 then
-	echo -e "[!] - ERROR : Missing Argument\nUsage : $0 [path_to_directory]" >2
+	echo -e "[!] - ERROR : Missing Argument\nUsage : $0 [path_to_directory]" >&2
 	exit 1
 else
 	if [[ ! -d "$1" ]] 
 	then
-		echo "[!] - ERROR : The specified Argument $1 is not a directory" >2
+		echo "[!] - ERROR : The specified Argument $1 is not a directory" >&2
 		exit 1
 	elif (ls -lA $1 | grep -qo docker-compose.yml)
 	then
